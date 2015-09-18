69db719b0c16ec9748943ebbbdd4af09ea9d8175
Dtan1293 <Dtan1293@gmail.com>
2015-09-18 15:22:55 -0700

    Added some text
    testing github

--- RFID.ino
+++ RFID.ino
@@ -40,7 +40,7 @@
 
 #define DBG		0
 
-#include <Energia.h>   	
+#include <Energia.h>   	        //CC3200 Specifically
 #include <stdlib.h>		//Processor specific definitions
 #include <stdio.h>
 #include "hardware.h"
